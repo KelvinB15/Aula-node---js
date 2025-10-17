29a444f9626c7713139e2be6ce34bc15d60f7a63
kelvin-trabalho <kelvinkelvinbarroso@gmail.com>
2025-10-17 15:31:00 -0300

    Organiza e adiciona arquivos de aulas anteriores

---
+++ NodeAula/01-fundamentos-nodejs/src/server.js
@@ -0,0 +1,16 @@
+import http from 'node:http'
+
+
+// Criar um usuário (name, email, senha)
+/* 
+request = importar -- vai receber
+response = exportar -- vai enviar
+*/
+
+const server = http.createServer((request, response) => {
+    return response.end('Hello World')
+})
+
+server.listen(3333)
+
+console.log
