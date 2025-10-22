59d3c25652b8bf9d1b1f648ca716b76e5afecda9
kelvin-trabalho <kelvinkelvinbarroso@gmail.com>
2025-10-22 09:47:12 -0300

    Estrutura da Aplicação - fim

--- 02-API-REST-NODEJS/src/server.ts
+++ 02-API-REST-NODEJS/src/server.ts
@@ -2,4 +2,18 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-console.loge(object)
+// GET, POST, PUT, PATCH, DELETE
+
+// http://localhost:3333/hello
+
+app.get('/hello', () => {
+  return 'Olá mundo'
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Runing!')
+  })
