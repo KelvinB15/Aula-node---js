a75fad75c9922cbe2d87ba11457debc8c9fb6ea8
kelvin-trabalho <kelvinkelvinbarroso@gmail.com>
2025-10-23 09:22:25 -0300

    Fim modulo - Implementando as rotas

--- 02-API-REST-NODEJS/src/env/index.ts
+++ 02-API-REST-NODEJS/src/env/index.ts
@@ -9,4 +9,28 @@ const envSchema = z.object({
   PORT: z.number().default(3333),
 })
 
-export const env = envSchema.safeParse(process.env)
+const _env = envSchema.safeParse(process.env)
+/*
+if (_env.success === false) {
+
+  console.error('Deu erro no env aqui', _env.error.flatten())
+
+  throw new Error('Deu erro no env aqui')
+}
+export const env = _env.data
+*/
+
+if (_env.success === false) {
+  const formatted = z.treeifyError(_env.error)
+
+  console.error('Erro no env, verifique se está sem conteudo')
+  console.dir(formatted, { depth: null })
+
+  throw new Error('Erro no env, verifique se está sem conteudo')
+}
+export const env = _env.data
+
+/*
+Aqui o zod está infromando o erro, caso coloque safeParse, ele não irá informa o erro
+export const env = envSchema.parse(process.env)
+*/
